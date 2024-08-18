750833a5a1e0f52d1744f80cb071e6c9a6aaa096
Rakibul Bhuiyan <80211974+RakibulBh@users.noreply.github.com>
2024-08-18 17:05:12 +0100

    Map loading and teleporting players to map

--- ServerScriptService/Round Module.lua
+++ ServerScriptService/Round Module.lua
@@ -17,4 +17,26 @@ module.ChooseMap = function (maps)
 	end
 end
 
+module.LoadMap = function (map)
+	local mapClone = map:Clone()
+	mapClone.Parent = game.workspace
+	mapClone.Name = "Map"
+end
+
+module.TeleportPlayers = function (players, map: Model)
+	local mapSpawns = map:WaitForChild("Spawns"):GetChildren()
+
+	for _, player in pairs(players) do
+		local character = player.Character
+		if character then
+			local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+			if humanoidRootPart then
+				local randomSpawn = mapSpawns[math.random(1, #mapSpawns)]
+				humanoidRootPart.CFrame = randomSpawn.CFrame + Vector3.new(0, 5, 0)
+			end
+		end
+	end
+end
+
+
 return module
